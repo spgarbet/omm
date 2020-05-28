861d9f532b8ede5aaa279ea5f22312ab2984dbc3
Shawn Garbett <shawn@garbett.org>
2020-05-28 11:11:55 -0500

    Added notes on ACCRE usage

--- test_delta.R
+++ test_delta.R
@@ -28,6 +28,11 @@ inline.time <- system.time(replicate(5000, delta_it_c(mm, mm.lag, gamma.mat)))
 # Rcpp interfers with inline and crashes R
 # Doing this last seems to work
 library(Rcpp)
+# For Local compile
+Sys.setenv(PKG_LIBS="-llapack")
+# For ACCRE compile
+#Sys.setenv(PKG_LIBS="-L${MKLROOT}/lib/intel64 -lmkl_rt -lpthread -lm -ldl")
+#Sys.setenv(PKG_CXXFLAGS="-I${MKLROOT}/include")
 sourceCpp("src/delta_it.cpp")
 
 rcppv     <- delta_it_cpp(mm, mm.lag, gamma.mat, length(mm.lag), 1e-4, 100, 0)
