920abb1ad4c9be4164f940b6c12289994c556d1b
Shawn Garbett <shawn@garbett.org>
2020-06-01 11:32:59 -0500

    Changed names in hmat_calc to be descriptive

--- hmat_calc_Rcpp.R
+++ hmat_calc_Rcpp.R
@@ -7,7 +7,7 @@ Sys.setenv(PKG_LIBS="-llapack")
 #Sys.setenv(PKG_LIBS="-L${MKLROOT}/lib/intel64 -lmkl_rt -lpthread -lm -ldl")
 #Sys.setenv(PKG_CXXFLAGS="-I${MKLROOT}/include")
 
-sourceCpp("~/rsch/Git/OMM/src/hmat_calc.cpp")
+sourceCpp("src/hmat_calc.cpp")
 
 hmat_calc <- function(Delta.vec, gamma.mat)
   hmat_calc_cpp(Delta.vec, gamma.mat) 
--- src/hmat_calc.cpp
+++ src/hmat_calc.cpp
@@ -6,34 +6,35 @@ using namespace Rcpp;
 
 // [[Rcpp::export()]]
 NumericMatrix hmat_calc_cpp(
-  NumericVector Deltavec,    // k-1
-  NumericMatrix gammamat // (k-1, k)
+  NumericVector Deltavec,  // k-1
+  NumericMatrix gammamat   // (k-1, k)
 )
 {
   int    km1=Deltavec.length();
   int    k=km1+1;      // K - 1
-  int    i,j;        // i denotes row, j denotes column,
-  double temp;
+  int    row,col;
+  double denom;
   
   NumericMatrix hmat(km1, k);
-    // Delta.mat   <- matrix(rep(Delta.vec,each=K), ncol=K, byrow=TRUE)
-    // hmat.num    <- exp(Delta.mat+gamma.mat)
-    // hmat.denom  <- 1+ colSums(hmat.num)
-    // hmat        <- sweep(hmat.num,2,hmat.denom,"/")
-    for(j=0; j<k; ++j)  // j is the column, outer-loop due to column-wise nature
-    { 
-      temp = 1.0; // Denominator for column
-      for(i=0; i<km1; ++i) // i is the row
-      {
-        // matrix memory is a column-wise in layout
-        // vector sequential in memory.
-        // i.e., all matrices appear as as.vector(matrix) in memory
-        // location = row + col*nrow 
-        hmat[i+j*km1] = exp(Deltavec[i]+gammamat[i+j*km1]);
-        temp += hmat[i+j*km1];
-      }
-      for(i=0; i<km1; ++i) hmat[i+j*km1] /= temp;
+
+  // Delta.mat   <- matrix(rep(Delta.vec,each=K), ncol=K, byrow=TRUE)
+  // hmat.num    <- exp(Delta.mat+gamma.mat)
+  // hmat.denom  <- 1 + colSums(hmat.num)
+  // hmat        <- sweep(hmat.num,2,hmat.denom,"/")
+  for(col=0; col<k; ++col)  // outer-loop due to column-wise nature of calc
+  { 
+    denom = 1.0; // Denominator for column
+    for(row=0; row<km1; ++row) 
+    {
+      // matrix memory is a column-wise in layout
+      // vector sequential in memory.
+      // i.e., all matrices appear as as.vector(matrix) in memory
+      // location = row + col*nrow 
+      hmat[row+col*km1] = exp(Deltavec[row]+gammamat[row+col*km1]);
+      denom += hmat[row+col*km1];
     }
+    for(row=0; row<km1; ++row) hmat[row+col*km1] /= denom;
+  }
  
   return(hmat);
 }
